47e2de706bcee49f0b13106dc64b762a054ee12c
raphael-gravagna <91827242+raphael-gravagna@users.noreply.github.com>
2022-01-17 16:57:20 +0100

    Add files via upload

---
+++ creer_article.php
@@ -0,0 +1,76 @@
+<?php 
+require_once 'config.php'; 
+
+$user = 'root';
+$pass = '';
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
+    $bdd->exec('SET NAMES "UTF8"');
+
+}
+
+catch (PDOException $e)
+{
+    print "Error: " . $e->getMessage() . "<br/>";
+    die();
+}
+//utiliser classe getAllInfos pour récup Id user
+
+
+$id = $_SESSION['user']['0']['id'];
+var_dump($id);
+
+if($id == 1337 || $id == 42 ) {
+    if(isset($_POST['envoyer']) && isset($_POST['titre']) && isset($_POST['article']) && isset($_POST['categorie'])) {
+        echo "Merci pour votre contibution";
+        $id_categorie = $_POST['categorie'];
+        echo $id_categorie;
+        $titre = $_POST['titre'];
+        $article = $_POST['article'];
+        $request = "INSERT INTO `articles`( `article`, `id_utilisateur`, `id_categorie`, `date`) VALUES ( '$article','$id',  '$id_categorie', NOW())";
+        $calcul = $bdd->prepare($request);
+        $calcul->execute();
+        $result = $calcul->fetchAll(PDO::FETCH_ASSOC);
+        
+    }
+    
+}
+else {
+    header("Location:./articles.php");
+}
+
+?>
+
+<body>
+        <div>
+             
+
+            <form action="" method="POST">
+               
+
+                <h1>
+                Article
+                 </h1>
+
+
+                 <textarea maxlength="2000" name="titre" cols="20" rows="1">Titre</textarea>
+
+                <h1>
+                </h1>
+                <textarea name="article" cols="52" rows="5">Créez votre article</textarea>
+
+                <input type="submit" id='submit' name="envoyer" value='Envoyer'>
+                <h1></h1>
+                
+                <select name="categorie">
+                    <option value="">choisissez une catégorie</option>
+                    <option value="1">Life Style</option>
+                    <option value="2">Street Wear</option>
+                    <option value="3">Jul</option>
+                    <option value="4">Road Style</option>
+                </select>
+            </form>
+        </div>
+    </body>
