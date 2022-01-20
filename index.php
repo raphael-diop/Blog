dd9e54e16d237c32fdd54a591e41606e49f562ec
raphael-diop <91830351+raphael-diop@users.noreply.github.com>
2022-01-20 13:08:22 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    require 'config.php';
+    $article = new article();
+    $art = $article->recuperationArticles();
+    //requete pour récupérer les 3 premier articles et les commentaires
+    
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,20 +21,23 @@
         <?php require 'header.php'; ?>
     </header>
     <main>
-        <div class="container">
-            <div class='slider'>
-                <img class='active' src="https://intrld.com/wp-content/uploads/2020/12/jul-beat-it-michael-jackson.png" alt="jul" />
-                <img src="https://www.reead.com/fr/wp-content/uploads/2019/09/claquettes-chaussettes-off-white.webp" alt="claquette chaussette" />
-                <img src="https://static.lexpress.fr/medias_11182/w_1000,h_563,c_fill,g_north/v1476370079/lopez-du-63_5725391.jpg" alt="Lopez" />
-                <img src="https://i.ytimg.com/vi/4vtAcnxJDAk/maxresdefault.jpg" alt="Scooter" />
-            </div>
+        <div class='slider'>
+            <img class='active' src="https://zupimages.net/up/22/03/9b3q.jpg" alt="jul" />
+            <img src="https://www.reead.com/fr/wp-content/uploads/2019/09/claquettes-chaussettes-off-white.webp" alt="claquette chaussette" />
+            <img src="https://static.lexpress.fr/medias_11182/w_1000,h_563,c_fill,g_north/v1476370079/lopez-du-63_5725391.jpg" alt="Lopez" />
+            <img src="https://i.ytimg.com/vi/4vtAcnxJDAk/maxresdefault.jpg" alt="Scooter" />
             <div class="container-btn">
-                <div class="btn-nav left">←</div>
-                <div class="btn-nav right">→</div>
+                <div class="btn-nav left">◄</div>
+                <div class="btn-nav right">►</div>
+                <script src="app.js"></script>
             </div>
         </div>
-
-        <script src="app.js"></script>
-
+            <div class="articles">
+                <?php foreach($art as $i){ 
+                    echo '<div class="article">' .$i['article']. '</div>';
+                } ?>
+            </div>
     </main>
+    
+
 </body>
